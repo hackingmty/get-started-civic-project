70fa6d8ac52e5a0db87406ad2526af935182f9e3
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-05-02 00:29:30 -0500

    Finished wire-layout, pre javascript commit

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,9 @@
 //= require_tree .
+jQuery(document).ready(function($) {
+
+    //var navbar = $('.navbar-static-top'),
+    //		distance = navbar.offset().top,
+    //    $window = $(window);
+
+
+});
