613d404cd74452fa43f1ecc58889808adc67dafd
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-05-05 21:09:23 -0500

    Add object followup

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -6,10 +6,20 @@ jQuery(document).ready(function($) {
     //    $window = $(window);
 });
 
+function getData(){
+  var output_data = new Object();
+  output_data["success_criteria"] = $('#sucess_criteria').val();
+  output_data["success_goal"] = $(#'#sucess_goal').val();
+  output_data["organization_name"] = $('#organization_name').val();
+  output_data["organization_owner"] = $('#organization_owner').val();
+  output_data["final_challenge"] = $('#final_challenge').val();
+  console.log(output_data)
+};
+
 
 function setText(incoming,outgoing) {
-    var string_incoming = "textarea" + incoming
-    var string_outgoing = "text_area"+ outgoing
+    var string_incoming = "textarea" + incoming;
+    var string_outgoing = "text_area"+ outgoing;
     $(outgoing).val($(incoming).val());
     return;              // The function returns the product of p1 and p2
-}
+};
