577d3c14eafa2178431b0107984408e6aa76a44b
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-05-05 23:12:12 -0500

    Automated commit at 2016-05-06 04:12:12 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -7,12 +7,17 @@ jQuery(document).ready(function($) {
 
 function getData(){
   var output_data = new Object();
-  output_data["success_criteria"] = $('#sucess_criteria').val();
-  output_data["success_goal"] = $(#'#sucess_goal').val();
+  output_data["project_name"] = $('#project_name').val();
+  output_data["success_criteria"] = $('#success_criteria').val();
+  output_data["success_goal"] = $('#success_goal').val();
+  output_data["drive_url"] = $('drive_url').val();
   output_data["organization_name"] = $('#organization_name').val();
   output_data["organization_owner"] = $('#organization_owner').val();
   output_data["final_challenge"] = $('#final_challenge').val();
-  console.log(output_data)
+  var url = "https://script.google.com/macros/s/AKfycbzKz8OOuKOwERBJHIdrthcP5HQlRDt6Di8snFOmy74O6OOXTYE/exec?project_name="+output_data["project_name"]+"&success_criteria="+output_data["success_criteria"]+"&success_goal="+output_data["success_goal"]+"&drive_url="+output_data["drive_url"]+"&organization_name="+output_data["organization_name"]+"&organization_owner="+output_data["organization_owner"]+"&final_challenge="+output_data["final_challenge"]+"&callback=?";
+  var jqxhr = $.getJSON( url, function() {
+  console.log( "success" );
+})
 };
 
 
